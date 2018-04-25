d340e5eef2b4e743e5f24fd95c407e40d524dde5
Jeremy Schaffer <jeremys@codenexus.org>
2018-04-25 14:01:00 -0700

    Fix spelling mistakes

--- app/Http/Controllers/UrlController.php
+++ app/Http/Controllers/UrlController.php
@@ -2,11 +2,12 @@
 
 namespace App\Http\Controllers;
 
+use App\Url;
 use Illuminate\Http\Request;
 
 class UrlController extends Controller
 {
-    public function manageUrl(Request $request, Store $session, Shortner $shortner)
+    public function manageUrl(Request $request, Store $session, Shortener $shortener)
     {
         if ($request->isMethod('post')) {
             $this->validate($request, [
@@ -14,11 +15,30 @@ class UrlController extends Controller
             ]);
         }
 
-        $this->saveAndFlash($request, $session, $shortner);
+        $this->saveAndFlash($request, $session, $shortener);
     }
 
-    private function saveAndFlash(Request $request, Store $session, Shortner $shortner) {
-        $message = $this->save($request, $shortner) ? 'Record saved' : 'Record NOT saved';
+    public function viewUrls(Store $session)
+    {
+        $urls = Url::all();
+
+        return view('url.view', [
+            'urls' => $urls,
+            'message' => $session->get('message'),
+        ]);
+    }
+
+    private function save(Request $request, Shortener $shortener)
+    {
+        $url = new Url();
+        $url->original_url = $request->input('url');
+        $url->shortened_url = $shortener->shorten($request->input('url'));
+
+        return $url->saveOrFail();
+    }
+
+    private function saveAndFlash(Request $request, Store $session, Shortener $shortener) {
+        $message = $this->save($request, $shortener) ? 'Record saved' : 'Record NOT saved';
         $session->flash('message', $message);
     }
 }
