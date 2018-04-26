079990297c9ce4e780cbe3b8525e0d3578ab5218
Jeremy Schaffer <jeremys@codenexus.org>
2018-04-25 20:45:06 -0700

    Add master layout and start views

---
+++ resources/views/url/view.blade.php
@@ -0,0 +1,37 @@
+@extends('layouts.master')
+
+@section('content')
+
+@if ($message !== null)
+    <div class="alert alert-success alert-dismissible" role="alert">{{ $message }}</div>
+@endif
+
+<div class="container-fluid">
+    <div class="row">
+        <table class="table table-striped table-bordered table-sm">
+            <thead>
+                <tr>
+                    <th>id</th>
+                    <th>shortened url</th>
+                    <th>original url</th>
+                    <th>created</th>
+                    <th>last updated</th>
+                </tr>
+            </thead>
+            <tbody>
+            @if (count($urls))
+                @foreach ($urls as $url)
+                    <tr>
+                        <td>{{ $url->id }}</td>
+                        <td><a href="{{ $url->shortened_url }}">{{ $url->shortened_url }}</a></td>
+                        <td><a href="{{ $url->original_url }}">{{ $url->original_url }}</a></td>
+                        <td>{{ $url->created_at }}</td>
+                        <td>{{ $url->updated_at }}</td>
+                    </tr>
+                @endforeach
+            @endif
+            </tbody>
+        </table>
+    </div>
+</div>
+@endsection
